5417100b537e5077f5b7bb49cc1ef72e4f0658b0
chenchangrao <chenchangrao@chinamcloud.com>
2019-11-28 19:17:40 +0800

    [int]

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const Koa = require('koa');
 const fs = require('fs');
 const app = new Koa();
 const router = require('./routers/index')
+// const http = require('http');
 
+// http.createServer((req,res) => {
+//   console.log(req);
+// }).listen(3000);
 
 app.use(router.routes()).use(router.allowedMethods())
 
--- routers/api.js
+++ routers/api.js
@@ -1,8 +1,10 @@
 const router = require('koa-router')();
 
-module.exports = router.get('/get', async (ctx) => {
+module.exports = router.post('/userLogin', async (ctx) => {
+  ctx.status = 200;
   ctx.body = {
     success: true,
+    message: '',
     data: {
       text: 'hello world'
     }
